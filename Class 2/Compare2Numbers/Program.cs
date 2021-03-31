3d77a89cedaa240d4b801ce25f3ccacefdf5e224
Nikola Stankovski <stankovski.n@hotmail.com>
2021-03-31 18:00:05 +0200

    Homework

---
+++ Class 2/Compare2Numbers/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Compare2Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number: ");
+            bool isOkNum1 = int.TryParse(Console.ReadLine(), out int num1);
+            Console.Write("Enter second number: ");
+            bool isOkNum2 = int.TryParse(Console.ReadLine(), out int num2);
+
+            if(isOkNum1 && isOkNum2)
+            {
+                if (num1 > num2)
+                {
+                    Console.WriteLine($"{num1} is bigger than {num2}");
+                    if(num1 % 2 == 0)
+                    {
+                        Console.WriteLine($"{num1} is an even number");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{num1} is an odd number");
+                    }
+                }
+                else if (num1 < num2)
+                {
+                    Console.WriteLine($"{num1} is smaller than {num2}");
+                    if (num2 % 2 == 0)
+                    {
+                        Console.WriteLine($"{num2} is an even number");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{num2} is an odd number");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("They are even");
+                    if (num1 % 2 == 0)
+                    {
+                        Console.WriteLine($"{num1} & {num2} are an even numbers");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{num1} & {num2} are an odd numbers");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input");
+            }
+        }
+    }
+}
---
+++ Class 2/SwitchStatement/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SwitchStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Write a number from 1 to 3: ");
+            bool isOkNum = int.TryParse(Console.ReadLine(), out int num);
+
+            if (isOkNum)
+            {
+                switch (num)
+                {
+                    case 1:
+                        Console.WriteLine("You got a new car");
+                        break;
+                    case 2:
+                        Console.WriteLine("You got a new plane");
+                        break;
+                    case 3:
+                        Console.WriteLine("You got a new bike");
+                        break;
+                    default:
+                        Console.WriteLine("Wrong number");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid unput");
+            }
+        }
+    }
+}
